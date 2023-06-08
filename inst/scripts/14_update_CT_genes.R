0c85ceccba2652011554314c92d7045b83fe005e
aloriot <axelle.loriot@uclouvain.be>
2023-06-08 11:07:59 +0200

    changed description of CT_genes update

--- inst/scripts/14_update_CT_genes.R
+++ inst/scripts/14_update_CT_genes.R
@@ -6,10 +6,16 @@ library("SingleCellExperiment")
 load("../../eh_data/CT_genes.rda")
 load("../../eh_data/testis_sce.rda")
 
-################################################################################
-## Add `testis_cell_type` column, specifying the testis cell-type showing the
-## highest median expression of each gene.
-################################################################################
+##########################################################################
+## Add 3 columns based on testis scRNAseq data:
+## - `percent_pos_testis_germcells` giving the percent of testis germ cells
+## in which the genes are detected (count > 0)
+## - `percent_pos_testis_somatic` giving the percent of testis somatic cells
+## in which the genes are detected (count > 0)
+## - `testis_cell_type` column, specifying the testis cell-type showing
+## the highest mean expression of each gene.
+##########################################################################
+
 CT_genes <- CT_genes %>%
   left_join(as_tibble(rowData(testis_sce)))
 
