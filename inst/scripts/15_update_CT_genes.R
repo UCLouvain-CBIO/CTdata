482608808803eff65c3c30814fb4239887ccf8ad
aloriot <axelle.loriot@uclouvain.be>
2023-07-05 18:50:52 +0200

    Simplified the CT_genes updates

--- inst/scripts/15_update_CT_genes.R
+++ inst/scripts/15_update_CT_genes.R
@@ -1,10 +1,23 @@
-## Code to prepare `CT_genes` dataset goes here
+## Code to update `CT_genes` dataset goes here
 
 library("tidyverse")
 library("SingleCellExperiment")
 
 load("../../eh_data/CT_genes.rda")
+load("../../eh_data/testis_sce.rda")
 load("../../eh_data/scRNAseq_HPA.rda")
+
+##########################################################################
+## Add the `testis_cell_type` column (based on testis scRNAseq data),
+## specifying the testis cell-type showing the highest mean expression
+## of each gene.
+## Remove genes for which testis_cell_type corresponds to a testis somatic
+## cell type.
+##########################################################################
+CT_genes <- CT_genes %>%
+  left_join(as_tibble(rowData(testis_sce)) %>%
+              dplyr::select(external_gene_name, testis_cell_type)) %>%
+  filter(!testis_cell_type %in% c( "Macrophage", "Endothelial", "Myoid", "Sertoli", "Leydig"))
 
 ##########################################################################
 ## Add the `Higher_in_somatic_cell_type` column (based on scRNAseq data
