c3a4b48a661eb85c1576f606b5a47d11ba4af7f5
Julie Devis <julie.devis@uclouvain.be>
2024-05-07 19:06:52 +0200

    Verification make_sc_HPA (no change in doc)

--- inst/scripts/080_make_scRNAseq_HPA.R
+++ inst/scripts/080_make_scRNAseq_HPA.R
@@ -22,9 +22,9 @@ rowdata <- data.frame("external_gene_name" = data$external_gene_name)
 rownames(rowdata) <- data$ensembl_gene_id
 
 cell_types <- c("Adipocyte_cells", "Blood_immune_cells", "Endocrine_cells",
-                "Endothelial_cells", "Germ_cells", "Glandular_cells", "Glial_cells",
-                "Mesenchymal_cells", "Pigment_cells", "Specialized_epithelial_cells",
-                "Trophoblast_cells")
+                "Endothelial_cells", "Germ_cells", "Glandular_cells",
+                "Glial_cells", "Mesenchymal_cells", "Pigment_cells",
+                "Specialized_epithelial_cells", "Trophoblast_cells")
 
 cell_types_by_group <- read_tsv(paste0("../extdata/", cell_types[1]), col_select = 1,
                                 col_names = c("Cell_type")) %>%
@@ -39,14 +39,16 @@ for (cell in cell_types[-1]){
 cell_types_by_group$Cell_type <- str_to_sentence(cell_types_by_group$Cell_type)
 
 # Rm "Hofbauer cells" (placental cells => confusing)
-cell_types_by_group <- cell_types_by_group %>% filter(Cell_type != "Hofbauer cells")
+cell_types_by_group <- cell_types_by_group %>%
+  filter(Cell_type != "Hofbauer cells")
 
 scRNAseq_HPA <- SingleCellExperiment(assays = list(TPM = mat[, cell_types_by_group$Cell_type]),
                                      colData = cell_types_by_group,
                                      rowData = rowdata)
 
 not_somatic_group <- c("Germ_cells", "Trophoblast_cells")
-somatic_groups <- unique(cell_types_by_group$group[!cell_types_by_group$group %in% not_somatic_group])
+somatic_groups <- unique(cell_types_by_group$group[
+  !cell_types_by_group$group %in% not_somatic_group])
 
 ##########################################################################
 ## Add 3 columns to the rowData:
@@ -58,8 +60,10 @@ somatic_groups <- unique(cell_types_by_group$group[!cell_types_by_group$group %i
 ## was found to express the gene at a higher level than any germ cell type
 ##########################################################################
 
-values_in_somatics <- as_tibble(assay(scRNAseq_HPA), rownames = "ensembl_gene_id") %>%
-  pivot_longer(names_to = "Cell_type", values_to = "counts", -ensembl_gene_id) %>%
+values_in_somatics <- as_tibble(assay(scRNAseq_HPA),
+                                rownames = "ensembl_gene_id") %>%
+  pivot_longer(names_to = "Cell_type", values_to = "counts",
+               -ensembl_gene_id) %>%
   left_join(as_tibble(colData(scRNAseq_HPA))) %>%
   filter(group %in% somatic_groups) %>%
   group_by(ensembl_gene_id, group) %>%
@@ -68,8 +72,10 @@ values_in_somatics <- as_tibble(assay(scRNAseq_HPA), rownames = "ensembl_gene_id
   dplyr::select(-group) %>%
   unique()
 
-values_in_germCells <- as_tibble(assay(scRNAseq_HPA), rownames = "ensembl_gene_id") %>%
-  pivot_longer(names_to = "Cell_type", values_to = "counts", -ensembl_gene_id) %>%
+values_in_germCells <- as_tibble(assay(scRNAseq_HPA),
+                                 rownames = "ensembl_gene_id") %>%
+  pivot_longer(names_to = "Cell_type", values_to = "counts",
+               -ensembl_gene_id) %>%
   left_join(as_tibble(colData(scRNAseq_HPA))) %>%
   filter(group == "Germ_cells") %>%
   group_by(ensembl_gene_id) %>%
@@ -80,11 +86,16 @@ rowdata <- as_tibble(rowData(scRNAseq_HPA), rownames = "ensembl_gene_id") %>%
   left_join(values_in_somatics) %>%
   left_join(values_in_germCells) %>%
   mutate(Higher_in_somatic_cell_type =
-           case_when((max_TPM_in_a_somatic_cell_type > 0 | max_in_germcells_group > 0) &
-                       max_TPM_in_a_somatic_cell_type > max_in_germcells_group ~ TRUE,
-                     (max_TPM_in_a_somatic_cell_type > 0 | max_in_germcells_group > 0) &
-                       max_TPM_in_a_somatic_cell_type <= max_in_germcells_group ~ FALSE,
-                     (max_TPM_in_a_somatic_cell_type == 0 & max_in_germcells_group == 0) ~ NA)) %>%
+           case_when((max_TPM_in_a_somatic_cell_type > 0 |
+                        max_in_germcells_group > 0) &
+                       max_TPM_in_a_somatic_cell_type > max_in_germcells_group
+                     ~ TRUE,
+                     (max_TPM_in_a_somatic_cell_type > 0 |
+                        max_in_germcells_group > 0) &
+                       max_TPM_in_a_somatic_cell_type <= max_in_germcells_group
+                     ~ FALSE,
+                     (max_TPM_in_a_somatic_cell_type == 0 &
+                        max_in_germcells_group == 0) ~ NA)) %>%
   as.data.frame()
 
 rownames(rowdata) <- rowdata$ensembl_gene_id
