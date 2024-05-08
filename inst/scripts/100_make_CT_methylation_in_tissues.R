22dd05289a445bd744673eab4a051db08aa19a1d
aloriot <axelle.loriot@uclouvain.be>
2024-05-08 12:59:23 +0200

    renamed files

--- inst/scripts/100_make_CT_methylation_in_tissues.R
+++ inst/scripts/100_make_CT_methylation_in_tissues.R
@@ -1,4 +1,4 @@
-## Code to prepare `methylation_in_tissues` dataset goes here
+## Code to prepare `CT_methylation_in_tissues` dataset goes here
 
 library(GenomicRanges)
 library(SummarizedExperiment)
@@ -6,7 +6,7 @@ library(Biostrings)
 library(tidyverse)
 library(liftOver)
 library(AnnotationHub)
-load("../extdata/all_genes.rda")
+load("../extdata/CT_list.rda")
 
 # Download human genome fasta file
 # download.file("http://ftp.ensembl.org/pub/release-104/fasta/homo_sapiens/dna/Homo_sapiens.GRCh38.dna.primary_assembly.fa.gz",
@@ -45,14 +45,29 @@ CpG_positions_gr <- makeGRangesFromDataFrame(CpG_positions,
                                              start.field = "Cytosine_fw",
                                              end.field = "Cytosine_rev")
 
+## Keep only regions around each CT TSS
+CT_promoter_regions <- CT_list %>%
+  mutate(start = transcription_start_site - 5000) %>%
+  mutate(stop = transcription_start_site + 5000) %>%
+  mutate(chromosome_name = paste0("chr", chromosome_name)) %>%
+  mutate(strand = case_when(strand == 1 ~ "+", strand == -1 ~ "-"))
 
+CT_list_prom_gr <- makeGRangesFromDataFrame(CT_promoter_regions,
+                                            keep.extra.columns = TRUE,
+                                            seqnames.field = "chromosome_name",
+                                            start.field = "start",
+                                            end.field = "stop")
+
+
+CpG_positions_gr <- subsetByOverlaps(CpG_positions_gr, CT_list_prom_gr)
 
 
 
 ################################################################################
-## Create a table with methylation values of all CpG in normal tissues
+## Create a table with methylation values of CpG located in CT promoters
+## in normal tissues
 ################################################################################
-met_in_tissues <- as_tibble(CpG_positions_gr) %>%
+CT_met_in_tissues <- as_tibble(CpG_positions_gr) %>%
   dplyr::rename(chr = seqnames) %>%
   dplyr::select(chr, CpG_pos)
 Encode_tissues <- c("adipose", "colon", "esophagus", "heart", "intestine",
@@ -110,7 +125,7 @@ for (cell in Encode_tissues) {
   names(bismark_merge) <- c("chr", "CpG_pos", cell)
 
   # Add methylation values for the tissue in CpG_methylation tibble
-  met_in_tissues <- met_in_tissues %>%
+  CT_met_in_tissues <- CT_met_in_tissues %>%
     left_join(bismark_merge, by = c("chr", "CpG_pos"))
   rm(bismark_merge)
 }
@@ -143,41 +158,22 @@ bismark_merge <- rbind(CG_pos_strand, CG_neg_strand) %>%
   group_by(chr, CpG_pos) %>%
   dplyr::summarise(sperm = mean(c(score), na.rm = TRUE))
 
-met_in_tissues <- met_in_tissues %>%
+CT_met_in_tissues <- CT_met_in_tissues %>%
   left_join(bismark_merge)
 
 
 #  Save as a RangedSummarizeExperiment
-met_normal_tissues_gr <-
-  makeGRangesFromDataFrame(met_in_tissues,
+CT_list_met_normal_tissues_gr <-
+  makeGRangesFromDataFrame(CT_met_in_tissues,
                            keep.extra.columns = FALSE,
                            seqnames.field = "chr",
                            start.field = "CpG_pos",
                            end.field = "CpG_pos")
 
-cpg_meth_healthy <- SummarizedExperiment(
-  assays = as.matrix(met_in_tissues[, -(1:2)]),
-  rowRanges = met_normal_tissues_gr)
+CT_methylation_in_tissues <- SummarizedExperiment(
+  assays = as.matrix(CT_met_in_tissues[, -(1:2)]),
+  rowRanges = CT_list_met_normal_tissues_gr)
 
-
-## Keep only regions around each gene TSS
-promoter_regions <- all_genes %>%
-  mutate(start = transcription_start_site - 5000) %>%
-  mutate(stop = transcription_start_site + 5000) %>%
-  mutate(chromosome_name = paste0("chr", chromosome_name)) %>%
-  mutate(strand = case_when(strand == 1 ~ "+", strand == -1 ~ "-"))
-
-prom_gr <- makeGRangesFromDataFrame(promoter_regions,
-                                            keep.extra.columns = TRUE,
-                                            seqnames.field = "chromosome_name",
-                                            start.field = "start",
-                                            end.field = "stop")
-
-
-methylation_in_tissues <- subsetByOverlaps(cpg_meth_healthy, prom_gr)
-
-
-save(methylation_in_tissues, file = "../../eh_data/methylation_in_tissues.rda",
+save(CT_methylation_in_tissues, file = "../../eh_data/CT_methylation_in_tissues.rda",
      compress = "xz",
      compression_level = 9)
-
