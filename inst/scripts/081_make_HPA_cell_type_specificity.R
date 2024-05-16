12a24665606a186ff7694963da61b46b4adc52f9
aloriot <axelle.loriot@uclouvain.be>
2024-05-16 11:10:27 +0200

    updated code to get the max expression in a somatic cell type (if any), the max expression in a germ cell and to calculate the ratio between these 2 values (will serve to filter out among testis-preferential genes that appear to be more expressed in a somatic cell type than in a germ cell type)

--- inst/scripts/081_make_HPA_cell_type_specificity.R
+++ inst/scripts/081_make_HPA_cell_type_specificity.R
@@ -10,49 +10,87 @@ proteinatlas <- read_tsv("../extdata/proteinatlas.tsv")
 
 
 # Use the column `RNA single cell type specific nTPM` to flag gene
-# as "not_testis_specific" genes if specific of a cell type other than a germ
-# cell or a placental cell and as "testis_specific" genes if specific of a germ
-# cell type or a placental cell
+# as "not_testis_specific" genes when they are specific of a somatic cell type
+HPA_single_cell_specificities <-
+  proteinatlas$`RNA single cell type specific nTPM`[
+    !is.na(proteinatlas$`RNA single cell type specific nTPM`)]
 
-cell_type_specificities_list <- strsplit(
-  proteinatlas$`RNA single cell type specific nTPM`, split = ";")
-names(cell_type_specificities_list) <- proteinatlas$Gene
+HPA_single_cell_specificities_list <- strsplit(
+  HPA_single_cell_specificities, split = ";")
+names(HPA_single_cell_specificities_list) <- proteinatlas[
+  !is.na(proteinatlas$`RNA single cell type specific nTPM`),]$Gene
+
 detected_specificities <- unique(gsub(
   pattern = ": .*", x = unlist(strsplit(
-    proteinatlas$`RNA single cell type specific nTPM`, split = ";")), ''))
+    HPA_single_cell_specificities, split = ";")), ''))
+
+germ_cell_types <- c("Spermatocytes", "Spermatogonia", "Early spermatids",
+                     "Late spermatids", "Oocytes")
 
 accepted_cell_types <- c("Spermatocytes", "Spermatogonia", "Early spermatids",
                          "Late spermatids", "Oocytes", "Syncytiotrophoblasts",
-                         "Cytotrophoblasts", "Extravillous trophoblasts", NA)
+                         "Cytotrophoblasts", "Extravillous trophoblasts")
 
 somatic_cell_types <- detected_specificities[!detected_specificities %in%
                                                accepted_cell_types]
 
-suspect <- names(grep(somatic_cell_types[1], cell_type_specificities_list,
-                      value = TRUE))
-for (tissue in somatic_cell_types[-1]){
-  tmp <- names(grep(tissue, cell_type_specificities_list, value = TRUE))
-  suspect <- c(suspect, tmp)
+
+x <- unlist(strsplit(HPA_single_cell_specificities_list[[1]], split = ";"))
+cells <- gsub(pattern = ":.*$", x = x, replacement = '')
+values <- gsub(pattern = "^.*: ", x = x, replacement = '')
+max_som <- tibble(cell_type = cells,
+                  nTPM = as.numeric(values)) %>%
+  arrange(desc(nTPM)) %>%
+  filter(cell_type %in% somatic_cell_types) %>%
+  head(1) %>%
+  pull(nTPM)
+max_germ <- tibble(cell_type = cells,
+                   nTPM = as.numeric(values)) %>%
+  arrange(desc(nTPM)) %>%
+  filter(cell_type %in% germ_cell_types) %>%
+  head(1) %>%
+  pull(nTPM)
+HPA_nTPM <- tibble(external_gene_name = names(HPA_single_cell_specificities_list[1]),
+       max_HPA_somatic = ifelse(length(max_som) > 0, max_som, 0),
+       max_HPA_germcell = ifelse(length(max_germ) > 0, max_germ, 0))
+
+for (i in seq_along(HPA_single_cell_specificities_list)[-1]) {
+  x <- unlist(strsplit(HPA_single_cell_specificities_list[[i]], split = ";"))
+  cells <- gsub(pattern = ":.*$", x = x, replacement = '')
+  values <- gsub(pattern = "^.*: ", x = x, replacement = '')
+  max_som <- tibble(cell_type = cells,
+                    nTPM = as.numeric(values)) %>%
+    arrange(desc(nTPM)) %>%
+    filter(cell_type %in% somatic_cell_types) %>%
+    head(1) %>%
+    pull(nTPM)
+  max_germ <- tibble(cell_type = cells,
+                     nTPM = as.numeric(values)) %>%
+    arrange(desc(nTPM)) %>%
+    filter(cell_type %in% germ_cell_types) %>%
+    head(1) %>%
+    pull(nTPM)
+  tmp <- tibble(external_gene_name = names(HPA_single_cell_specificities_list[i]),
+                max_HPA_somatic = ifelse(length(max_som) > 0, max_som, 0),
+                max_HPA_germcell = ifelse(length(max_germ) > 0, max_germ, 0))
+  HPA_nTPM <- rbind(HPA_nTPM, tmp)
 }
-suspect_HPA <- unique(suspect)
+
+HPA_nTPM <- HPA_nTPM[!duplicated(HPA_nTPM$external_gene_name),]
 
 HPA_cell_type_specificities <- tibble(
   ensembl_gene_id = rownames(GTEX_data),
   external_gene_name = rowData(GTEX_data)$external_gene_name) %>%
-  mutate(not_detected_in_somatic_HPA = case_when(
-    !external_gene_name %in% proteinatlas$Gene ~ NA,
-    external_gene_name %in% suspect_HPA ~ FALSE,
-    external_gene_name %in% proteinatlas$Gene &
-      !external_gene_name %in% suspect_HPA ~ TRUE)) %>%
   left_join(
     proteinatlas %>%
       dplyr::rename(ensembl_gene_id = Ensembl) %>%
-      dplyr::select(ensembl_gene_id, `RNA single cell type specific nTPM`)) %>%
-  dplyr::rename(HPA_RNA_single_cell_type_specific_nTPM =
-                  `RNA single cell type specific nTPM`) %>%
-  dplyr::select(ensembl_gene_id, external_gene_name,
-                HPA_RNA_single_cell_type_specific_nTPM,
-                not_detected_in_somatic_HPA)
+      dplyr::rename(HPA_scRNAseq_celltype_specific_nTPM = `RNA single cell type specific nTPM`) %>%
+      dplyr::select(ensembl_gene_id, HPA_scRNAseq_celltype_specific_nTPM)) %>%
+  left_join(HPA_nTPM) %>%
+  mutate(not_detected_in_somatic_HPA = case_when(
+    max_HPA_somatic == 0 ~ TRUE,
+    max_HPA_somatic > 0 ~ FALSE)) %>%
+  mutate(HPA_ratio_germ_som = max_HPA_germcell / max_HPA_somatic)
 
 save(HPA_cell_type_specificities,
      file = "../../eh_data/HPA_cell_type_specificities.rda",
