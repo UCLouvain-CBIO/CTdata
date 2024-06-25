da4a89dfa1baa9bb6baf4e16771616175fb9805f
Julie Devis <julie.devis@uclouvain.be>
2024-06-25 16:15:10 +0200

    added zhu data

--- inst/scripts/150_make_embryo_sce_Zhu.R
+++ inst/scripts/150_make_embryo_sce_Zhu.R
@@ -1,6 +1,8 @@
 ## Code to prepare `embryo_sce_Zhu` dataset goes here
 
 library(tidyverse)
+library(biomaRt)
+library(SingleCellExperiment)
 
 
 ## Data from Single Cell DNA Methylome Sequencing of Human Preimplantation
@@ -35,42 +37,38 @@ for (file in files[-1]){
 }
 
 
-
+coldata <- tibble(sample = colnames(count_data[,-1])) %>%
+  mutate(embryo = gsub(x = sample, pattern = "-\\w*$", ""))
 
 ## Determine gender of blastocysts
 
-
 # identify Y-linked genes highly expressed
-load("~/cluster/CBIO-templates/ensembl_2_geneName/transcripts_infos.rda")
+ensembl <- biomaRt::useDataset("hsapiens_gene_ensembl", useMart("ensembl"))
+
+attributes_vector <-  c("ensembl_gene_id", "external_gene_name",
+                        "ensembl_transcript_id", "external_transcript_name",
+                        "chromosome_name", "strand", "transcript_start",
+                        "transcript_end", "transcription_start_site",
+                        "transcript_length", "transcript_tsl",
+                        "transcript_gencode_basic", "transcript_appris",
+                        "transcript_mane_select", "transcript_biotype",
+                        "transcript_is_canonical" )
+
+transcripts_infos <-  as_tibble(biomaRt::getBM(attributes = attributes_vector,
+                                               mart = ensembl))
 Y_linked <- transcripts_infos %>%
   filter(external_gene_name %in% count_data$gene) %>%
   filter(chromosome_name == "Y") %>%
-  pull(external_gene_name) %>% unique()
+  pull(external_gene_name) %>%
+  unique()
+
 # Y-linked genes  expressed in the dataset
 mat <- as.matrix(count_data[,-1])
 rownames(mat) <- count_data$gene
 Y_linked_expressed <- Y_linked[Y_linked %in% rownames(mat)[rowSums(mat) > 10]]
-count_data %>%
-  filter(gene %in% Y_linked_expressed) %>%
-  pivot_longer(names_to = "sample", values_to = "FPKM", -gene) %>%
-  ggplot(aes(x = sample, y = FPKM)) +
-  geom_col() +
-  facet_wrap(~ gene)
-# RPS4Y1 seems the best gene to determine gender
 
-coldata <- tibble(sample = colnames(count_data[,-1])) %>%
-  mutate(embryo = gsub(x = sample, pattern = "-\\w*$", ""))
-coldata
-
-
-count_data %>%
-  filter(gene %in% c("RPS4Y1")) %>%
-  pivot_longer(names_to = "sample", values_to = "FPKM", -gene) %>%
-  left_join(coldata) %>%
-  ggplot(aes(x = sample, y = FPKM)) +
-  geom_col() +
-  facet_wrap(~ embryo, scales = "free_x", ncol = 8) +
-  theme(axis.text.x = element_text(size = 6, angle = 90))
+# After analysis, RPS4Y1 seems to be the best gene to determine gender
+# If the mean expression of RPS4Y1 is higher than 50 FPKM, it's a male sample
 
 embryo_gender <- count_data %>%
   filter(gene %in% c("RPS4Y1")) %>%
@@ -78,20 +76,18 @@ embryo_gender <- count_data %>%
   left_join(coldata) %>%
   group_by(embryo) %>%
   summarize(mean_RPS4Y1 = mean(FPKM)) %>%
-  mutate(gender = ifelse(mean_RPS4Y1 > 50, "M", "F")) %>%
-  dplyr::select(embryo, gender)
+  mutate(sex = ifelse(mean_RPS4Y1 > 50, "M", "F")) %>%
+  dplyr::select(embryo, sex)
 
 coldata <- coldata %>%
-  left_join(embryo_gender)
+  left_join(embryo_gender) %>%
+  mutate(genotype = ifelse(sex == "M", "XY", "XX")) %>%
+  mutate(stage = "blastocyst")
 
+embryo_sce_Zhu <- SingleCellExperiment(assays = mat[, coldata$sample],
+                                       colData = coldata)
 
+save(embryo_sce_Zhu, file = "../../eh_data/embryo_sce_Zhu.rda",
+     compress = "xz",
+     compression_level = 9)
 
-
-# save sce
-
-
-embryo_sce <- SingleCellExperiment(assays = mat[, coldata$sample],
-                                   colData = coldata)
-embryo_sce
-
-save(embryo_sce, file = "../data/embryo_sce_Zhu_datset.rda")
