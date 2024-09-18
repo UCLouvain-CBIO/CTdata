8f13eddd689d4eb8579829ae101b096708a9a126
Julie Devis <julie.devis@uclouvain.be>
2024-09-18 19:27:24 +0200

    Correction in upload file

--- inst/scripts/upload.R
+++ inst/scripts/upload.R
@@ -38,25 +38,25 @@ dest <- "CTdata/eh_data/v3/CT_methylation_in_tissues.rda"
 storage_multiupload(container, src = src, dest = dest)
 
 ## ------------------------------------------
-## 2024-08
+## 2024-08/09
 
-src <- c("/storage/research/dduv/cbio-lg/cluster/Packages/CTdata/eh_data/all_genes.rda",
-         "/storage/research/dduv/cbio-lg/cluster/Packages/CTdata/eh_data/CCLE_correlation_matrix.rda",
-         "/storage/research/dduv/cbio-lg/cluster/Packages/CTdata/eh_data/CCLE_data.rda",
-         "/storage/research/dduv/cbio-lg/cluster/Packages/CTdata/eh_data/CT_genes.rda",
+src <- c("/storage/research/dduv/cbio-lg/cluster/Packages/CTdata/eh_data/CCLE_data.rda",
          "/storage/research/dduv/cbio-lg/cluster/Packages/CTdata/eh_data/DAC_treated_cells_multimapping.rda",
          "/storage/research/dduv/cbio-lg/cluster/Packages/CTdata/eh_data/DAC_treated_cells.rda",
-         "/storage/research/dduv/cbio-lg/cluster/Packages/CTdata/eh_data/FGC_sce.rda",
          "/storage/research/dduv/cbio-lg/cluster/Packages/CTdata/eh_data/GTEX_data.rda",
+         "/storage/research/dduv/cbio-lg/cluster/Packages/CTdata/eh_data/normal_tissues_multimapping_data.rda",
+         "/storage/research/dduv/cbio-lg/cluster/Packages/CTdata/eh_data/scRNAseq_HPA.rda",
+         "/storage/research/dduv/cbio-lg/cluster/Packages/CTdata/eh_data/TCGA_TPM.rda",
+         "/storage/research/dduv/cbio-lg/cluster/Packages/CTdata/eh_data/testis_sce.rda",
+         "/storage/research/dduv/cbio-lg/cluster/Packages/CTdata/eh_data/CT_genes.rda",
+         "/storage/research/dduv/cbio-lg/cluster/Packages/CTdata/eh_data/CCLE_correlation_matrix.rda",
+         "/storage/research/dduv/cbio-lg/cluster/Packages/CTdata/eh_data/all_genes.rda",
+         "/storage/research/dduv/cbio-lg/cluster/Packages/CTdata/eh_data/FGC_sce.rda",
          "/storage/research/dduv/cbio-lg/cluster/Packages/CTdata/eh_data/HPA_cell_type_specificities.rda",
          "/storage/research/dduv/cbio-lg/cluster/Packages/CTdata/eh_data/mean_methylation_in_tissues.rda",
          "/storage/research/dduv/cbio-lg/cluster/Packages/CTdata/eh_data/methylation_in_tissues.rda",
-         "/storage/research/dduv/cbio-lg/cluster/Packages/CTdata/eh_data/normal_tissues_multimapping_data.rda",
-         "/storage/research/dduv/cbio-lg/cluster/Packages/CTdata/eh_data/oocytes_sce.rda",
-         "/storage/research/dduv/cbio-lg/cluster/Packages/CTdata/eh_data/scRNAseq_HPA.rda",
+        "/storage/research/dduv/cbio-lg/cluster/Packages/CTdata/eh_data/oocytes_sce.rda",
          "/storage/research/dduv/cbio-lg/cluster/Packages/CTdata/eh_data/TCGA_methylation.rda",
-         "/storage/research/dduv/cbio-lg/cluster/Packages/CTdata/eh_data/TCGA_TPM.rda",
-         "/storage/research/dduv/cbio-lg/cluster/Packages/CTdata/eh_data/testis_sce.rda",
          "/storage/research/dduv/cbio-lg/cluster/Packages/CTdata/eh_data/embryo_sce_Zhu.rda",
          "/storage/research/dduv/cbio-lg/cluster/Packages/CTdata/eh_data/embryo_sce_Petropoulos.rda",
          "/storage/research/dduv/cbio-lg/cluster/Packages/CTdata/eh_data/hESC_data.rda",
